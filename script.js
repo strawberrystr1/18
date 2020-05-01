a0885fd10b684ee11bdf2da146e4e754ea774d59
Andrei Shakh <andreishakhs@gmail.com>
2020-05-01 13:55:21 +0300

    lesson23

--- script.js
+++ script.js
@@ -290,20 +290,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
         const calcItems = document.querySelectorAll('input[type=number]');
         console.log('calcItems: ', calcItems);
-       
-        calcItems.forEach((item) => {
+
+        calcItems.forEach(item => {
 
             item.removeAttribute('type');
             item.addEventListener('input', () => {
                 item.value = item.value.replace(/\D/, '');
             });
-    
+
         });
-        
+
 
 
     };
 
+    //калькулятор
+
+    const calculator = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.vqlue - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+
+            if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            target.matches('.calc-day') || target.matches('.calc-count')) {
+                countSum();
+            }
+
+
+        });
+
+    };
+
+
+    calculator(100);
     calcalute();
     commandChange();
     slider();
